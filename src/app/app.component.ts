294c48104b561622bcc8e841ecf15b10efac4a91
nirupamaa jaya <nirustanite@gmail.com>
2020-05-03 12:11:17 +0200

    card display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cards';
+
+  posts = [
+    { 
+      title: "Neat Tree",
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this near tree today'
+    },
+    { 
+      title: "Snowy Mountain",
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here ias a picture of snowy mountain'
+    },
+    { 
+      title: "Mountain Biking",
+      imageUrl: 'assets/biking.jpeg',
+      username: 'Biking122',
+      content: 'I did some biking today'
+    }
+  ]
 }
